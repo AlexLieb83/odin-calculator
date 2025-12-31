5242522a5700abd039489aa9395bfab11991cbdf
AlexLieb83 <alexliebhaber1992@gmail.com>
2025-12-31 10:38:40 -0600

    adding operation functions to program

---
+++ script.js
@@ -0,0 +1,39 @@
+let input1;
+let input2;
+let inputOperator;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  if (operator == "+") {
+    return add(num1, num2);
+  } else if (operator == "-") {
+    return subtract(num1, num2);
+  } else if (operator == "*") {
+    return multiply(num1, num2);
+  } else if (operator == "/") {
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate("*", 5, 7));
+
+//click num, click operation, click other number
+
+//store num1, operation function, click num2
+
+//if click add button, then run add function
